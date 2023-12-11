96045c2a46909139c1d7f1bb88d14cd1484c1f61
Arash Altafi <arashaltafi1377@gmail.com>
2023-12-11 23:45:22 +0330

    a

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <Router>
+<Router basename='/'>
     <React.StrictMode>
       <App />
     </React.StrictMode>
