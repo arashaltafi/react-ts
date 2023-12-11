9a9a76045cb7a571a7e486a9300d415609864052
آرش الطافي <0021328374@nahad.ir>
2023-12-11 03:44:50 -0800

    a

--- src/pages/CustomIDB.tsx
+++ src/pages/CustomIDB.tsx
@@ -10,6 +10,7 @@ const CustomIDB = () => {
 
     const [todos, setTodos] = useState<Todo[]>([]);
     const [newTodo, setNewTodo] = useState<string>('');
+    const [editingTodo, setEditingTodo] = useState<Todo | null>(null);
 
     useEffect(() => {
         const openDatabase = async () => {
@@ -34,6 +35,34 @@ const CustomIDB = () => {
         setNewTodo('');
     };
 
+    const editTodo = (todo: Todo) => {
+        setEditingTodo(todo);
+        setNewTodo(todo.text);
+    };
+
+    const updateTodo = async () => {
+        if (!editingTodo) return;
+
+        const db = await openDB('todos', 1);
+        const updatedTodo = { ...editingTodo, text: newTodo };
+
+        await db.put('todos', updatedTodo);
+        const updatedTodos = todos.map((todo) =>
+            todo.id === editingTodo.id ? updatedTodo : todo
+        );
+
+        setTodos(updatedTodos);
+        setNewTodo('');
+        setEditingTodo(null);
+    };
+
+    const deleteTodo = async (id: number) => {
+        const db = await openDB('todos', 1);
+        await db.delete('todos', id);
+        const updatedTodos = todos.filter((todo) => todo.id !== id);
+        setTodos(updatedTodos);
+    };
+
     return (
         <div>
             <h1 className='title mt-8'>Todo App</h1>
@@ -41,7 +70,11 @@ const CustomIDB = () => {
                 className='mt-32 flex flex-col items-center justify-center gap-8'
                 onSubmit={(e) => {
                     e.preventDefault();
-                    addTodo();
+                    if (editingTodo) {
+                        updateTodo();
+                    } else {
+                        addTodo();
+                    }
                 }}
             >
                 <input
@@ -51,12 +84,16 @@ const CustomIDB = () => {
                     onChange={(e) => setNewTodo(e.target.value)}
                     placeholder="Enter a new todo"
                 />
-                <button className='btnSuccess' type="submit">Add Todo</button>
+                <button className="btnSuccess">{editingTodo ? 'Update Todo' : 'Add Todo'}</button>
             </form>
             <ul className='subtitle mt-8'>
                 {todos.map((todo) => (
                     <>
-                        <li className='m-4' key={todo.id}>{todo.text}</li>
+                        <div className='flex flex-row items-center justify-center'>
+                            <button className='btnError p-2 text-lg m-4' onClick={() => editTodo(todo)}>Edit</button>
+                            <li className='bg-green-500 rounded-lg text-white p-3 text-lg m-4' key={todo.id}>{todo.text}</li>
+                            <button className='btnError p-2 text-lg m-4' onClick={() => deleteTodo(todo.id)}>Delete</button>
+                        </div>
                         <hr />
                     </>
                 ))}
