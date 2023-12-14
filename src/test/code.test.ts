561f1306ab7e2e18de4b52a940e853e5d16bd68f
Arash Altafi <arashaltafi1377@gmail.com>
2023-12-14 17:51:09 +0330

    a

--- src/test/code.test.ts
+++ src/test/code.test.ts
@@ -1,16 +1,58 @@
-import { gte, lte } from './code';
+import { gte, lte, welcomeMessage, usersList, getUser } from './code';
 
-test('first test', () => {
-    expect(1).toBe(1);
-    // throw new Error('error');
-});
+describe('code test', () => {
+    test('first test', () => {
+        expect(1).toBe(1);
+        // throw new Error('error');
+    });
 
-test('gte', () => {
-    expect(gte(1, 1)).toBe(true);
-    expect(gte(1, 2)).toBe(false);
-    expect(gte(2, 1)).toBe(true);
+    test('gte', () => {
+        expect(gte(1, 1)).toBe(true);
+        expect(gte(1, 2)).toBe(false);
+        expect(gte(2, 1)).toBe(true);
+    });
 
-    expect(lte(1, 1)).toBe(true);
-    expect(lte(1, 2)).toBe(true);
-    expect(lte(2, 1)).toBe(false);
-});
+    test('lte', () => {
+        expect(lte(1, 1)).toBe(true);
+        expect(lte(1, 2)).toBe(true);
+        expect(lte(2, 1)).toBe(false);
+    })
+
+    test('welcomeMessage', () => {
+        expect(welcomeMessage('Arash')).toBe('Hello Arash, Welcome to my app');
+        expect(welcomeMessage('')).toBe('Please enter a value');
+        expect(welcomeMessage(null)).toBe('Please enter a value');
+        expect(welcomeMessage(NaN)).toBe('Please enter a value');
+        expect(welcomeMessage(undefined)).toBe('Please enter a value');
+        expect(welcomeMessage(0)).toBe('Please enter a value');
+    })
+
+    test('usersList', () => {
+        expect(usersList(27)).toEqual([
+            {
+                id: 2,
+                name: 'Amir',
+                age: 30
+            }
+        ]);
+        expect(usersList(0)).toEqual([]);
+        expect(usersList("something else than a number")).toEqual([]);
+        expect(usersList(null)).toEqual([]);
+        expect(usersList(undefined)).toEqual([]);
+        expect(usersList(NaN)).toEqual([]);
+    })
+
+    test('getUser', () => {
+        expect(getUser('Arash')).toMatchObject({
+            id: 1,
+            name: 'Arash'
+        });
+        expect(getUser('Arash')).toHaveProperty('name');
+        expect(getUser('Arash')).toHaveProperty('id', 1);
+        expect(getUser('')).toEqual(null);
+        expect(getUser(null)).toEqual(null);
+        expect(getUser(NaN)).toEqual(null);
+        expect(getUser(undefined)).toEqual(null);
+        expect(getUser(0)).toEqual(null);
+    })
+})
--- src/test/code.ts
+++ src/test/code.ts
@@ -10,4 +10,52 @@ const lte = (a: number, b: number) => {
     return true;
 }
 
-export { gte, lte }
+const welcomeMessage = (name: any) => {
+    if (!name) return 'Please enter a value';
+    if (typeof name !== 'string') return 'Please enter a value';
+    if (name === '') return 'Please enter a value';
+    if (name === null) return 'Please enter a value';
+    if (name === undefined) return 'Please enter a value';
+
+    return `Hello ${name}, Welcome to my app`;
+}
+
+const usersList = (age: any) => {
+    if (!age) return [];
+    if (typeof age !== 'number') return []; 
+    if (age < 0) return [];
+    if (age > 100) return [];
+
+
+    const users = [
+        {
+            id: 1,
+            name: 'Arash',
+            age: 26
+        },
+        {
+            id: 2,
+            name: 'Amir',
+            age: 30
+        }
+    ]
+
+    return users.filter((user) => {
+        return user.age >= age
+    })
+}
+
+const getUser = (name: any) => {
+    if (!name) return null;
+    if (typeof name !== 'string') return null;
+    if (name === '') return null;
+    if (name === null) return null;
+    if (name === undefined) return null;
+
+    return {
+        id: 1,
+        name: 'Arash',
+    }
+}
+
+export { gte, lte, welcomeMessage, usersList, getUser }
