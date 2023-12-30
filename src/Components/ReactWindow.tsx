16a6f90e9a889c738fa048f1f4d97a5e1f0f4eb6
آرش الطافي <0021328374@nahad.ir>
2023-12-30 12:43:37 +0330

    a

--- src/Components/ReactWindow.tsx
+++ src/Components/ReactWindow.tsx
@@ -1,21 +1,31 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { FixedSizeList as List } from 'react-window';
+import { faker } from '@faker-js/faker/locale/fa';
+import Divider from './Divider';
 
 const ReactWindow = () => {
 
     const [count, setCount] = useState<{
         id: number;
         name: string;
+        sex: string;
         bio: string;
+        job: string;
+        email: string;
+        avatar: string;
     }[]>([])
 
     const addNewItem = () => {
         setCount(
-            [...Array(100).keys()].map(key => {
+            [...Array(1000).keys()].map(key => {
                 return {
                     id: key,
-                    name: 'name',
-                    bio: 'bio'
+                    name: faker.person.fullName(),
+                    sex: faker.person.sex(),
+                    bio: faker.person.bio(),
+                    job: faker.person.jobTitle(),
+                    email: faker.internet.email(),
+                    avatar: faker.image.avatar()
                 }
             })
         )
@@ -31,7 +41,7 @@ const ReactWindow = () => {
                 <List
                     height={1000}
                     itemCount={count.length}
-                    itemSize={50}
+                    itemSize={350}
                     width={1000}
                 >
                     {
@@ -39,8 +49,14 @@ const ReactWindow = () => {
                             <>
                                 {isScrolling ? 'Scrolling' : ''}
                                 <ul style={style}>
-                                    <li>ID {count[index].id}</li>
-                                    <li>Name {count[index].name}</li>
+                                    <li>آیدی: {count[index].id}</li>
+                                    <li>نام: {count[index].name}</li>
+                                    <li>مرد یا زن: {count[index].sex}</li>
+                                    <li>بیو: {count[index].bio}</li>
+                                    <li>شغل: {count[index].job}</li>
+                                    <li>ایمیل: {count[index].email}</li>
+                                    <img className='w-28 h-28 mx-auto rounded-2xl' src={count[index].avatar} alt="User Avatar" />
+                                    <Divider />
                                 </ul>
                             </>
                     }
