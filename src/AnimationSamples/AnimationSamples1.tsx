efe32d175f0b3b618c8f3083e18bea92ad370168
arashaltafi <altafi.a@nahad.ir>
2024-01-15 09:01:13 +0330

    a

--- src/AnimationSamples/AnimationSamples1.tsx
+++ src/AnimationSamples/AnimationSamples1.tsx
@@ -1,9 +1,299 @@
-const AnimationSamples1 = () => {
-  return (
-    <div className="w-full h-screen py-8 flex flex-col gap-16">
-      <h1 className='title'>AnimationSamples 1</h1>
-      <div className='w-full h-full flex items-center justify-center flex-col gap-8'>
+import { useState } from "react"
 
+//https://tools.css-tricks.ir/animate.css/
+const AnimationSamples1 = () => {
+  const [isAddInfiniteAll, setIsAddInfiniteAll] = useState<boolean>(false)
+
+  const [isAddInfinite1, setIsAddInfinite1] = useState<boolean>(false)
+  const [isAddInfinite2, setIsAddInfinite2] = useState<boolean>(false)
+  const [isAddInfinite3, setIsAddInfinite3] = useState<boolean>(false)
+  const [isAddInfinite4, setIsAddInfinite4] = useState<boolean>(false)
+  const [isAddInfinite5, setIsAddInfinite5] = useState<boolean>(false)
+  const [isAddInfinite6, setIsAddInfinite6] = useState<boolean>(false)
+  const [isAddInfinite7, setIsAddInfinite7] = useState<boolean>(false)
+  const [isAddInfinite8, setIsAddInfinite8] = useState<boolean>(false)
+  const [isAddInfinite9, setIsAddInfinite9] = useState<boolean>(false)
+  const [isAddInfinite10, setIsAddInfinite10] = useState<boolean>(false)
+  const [isAddInfinite11, setIsAddInfinite11] = useState<boolean>(false)
+  const [isAddInfinite12, setIsAddInfinite12] = useState<boolean>(false)
+  const [isAddInfinite13, setIsAddInfinite13] = useState<boolean>(false)
+  const [isAddInfinite14, setIsAddInfinite14] = useState<boolean>(false)
+  const [isAddInfinite15, setIsAddInfinite15] = useState<boolean>(false)
+  const [isAddInfinite16, setIsAddInfinite16] = useState<boolean>(false)
+  const [isAddInfinite17, setIsAddInfinite17] = useState<boolean>(false)
+  const [isAddInfinite18, setIsAddInfinite18] = useState<boolean>(false)
+  const [isAddInfinite19, setIsAddInfinite19] = useState<boolean>(false)
+  const [isAddInfinite20, setIsAddInfinite20] = useState<boolean>(false)
+  const [isAddInfinite21, setIsAddInfinite21] = useState<boolean>(false)
+  const [isAddInfinite22, setIsAddInfinite22] = useState<boolean>(false)
+  const [isAddInfinite23, setIsAddInfinite23] = useState<boolean>(false)
+  const [isAddInfinite24, setIsAddInfinite24] = useState<boolean>(false)
+  const [isAddInfinite25, setIsAddInfinite25] = useState<boolean>(false)
+  const [isAddInfinite26, setIsAddInfinite26] = useState<boolean>(false)
+  const [isAddInfinite27, setIsAddInfinite27] = useState<boolean>(false)
+  const [isAddInfinite28, setIsAddInfinite28] = useState<boolean>(false)
+  const [isAddInfinite29, setIsAddInfinite29] = useState<boolean>(false)
+  const [isAddInfinite30, setIsAddInfinite30] = useState<boolean>(false)
+  const [isAddInfinite31, setIsAddInfinite31] = useState<boolean>(false)
+  const [isAddInfinite32, setIsAddInfinite32] = useState<boolean>(false)
+  const [isAddInfinite33, setIsAddInfinite33] = useState<boolean>(false)
+  const [isAddInfinite34, setIsAddInfinite34] = useState<boolean>(false)
+  const [isAddInfinite35, setIsAddInfinite35] = useState<boolean>(false)
+  const [isAddInfinite36, setIsAddInfinite36] = useState<boolean>(false)
+  const [isAddInfinite37, setIsAddInfinite37] = useState<boolean>(false)
+  const [isAddInfinite38, setIsAddInfinite38] = useState<boolean>(false)
+  const [isAddInfinite39, setIsAddInfinite39] = useState<boolean>(false)
+  const [isAddInfinite40, setIsAddInfinite40] = useState<boolean>(false)
+  const [isAddInfinite41, setIsAddInfinite41] = useState<boolean>(false)
+  const [isAddInfinite42, setIsAddInfinite42] = useState<boolean>(false)
+  const [isAddInfinite43, setIsAddInfinite43] = useState<boolean>(false)
+  const [isAddInfinite44, setIsAddInfinite44] = useState<boolean>(false)
+  const [isAddInfinite45, setIsAddInfinite45] = useState<boolean>(false)
+  const [isAddInfinite46, setIsAddInfinite46] = useState<boolean>(false)
+  const [isAddInfinite47, setIsAddInfinite47] = useState<boolean>(false)
+  const [isAddInfinite48, setIsAddInfinite48] = useState<boolean>(false)
+  const [isAddInfinite49, setIsAddInfinite49] = useState<boolean>(false)
+  const [isAddInfinite50, setIsAddInfinite50] = useState<boolean>(false)
+  const [isAddInfinite51, setIsAddInfinite51] = useState<boolean>(false)
+
+  const toggleInfinite = (num: number) => {
+    switch (num) {
+      case 1:
+        setIsAddInfinite1(!isAddInfinite1)
+        break;
+      case 2:
+        setIsAddInfinite2(!isAddInfinite2)
+        break;
+      case 3:
+        setIsAddInfinite3(!isAddInfinite3)
+        break;
+      case 4:
+        setIsAddInfinite4(!isAddInfinite4)
+        break;
+      case 5:
+        setIsAddInfinite5(!isAddInfinite5)
+        break;
+      case 6:
+        setIsAddInfinite6(!isAddInfinite6)
+        break;
+      case 7:
+        setIsAddInfinite7(!isAddInfinite7)
+        break;
+      case 8:
+        setIsAddInfinite8(!isAddInfinite8)
+        break;
+      case 9:
+        setIsAddInfinite9(!isAddInfinite9)
+        break;
+      case 10:
+        setIsAddInfinite10(!isAddInfinite10)
+        break;
+      case 11:
+        setIsAddInfinite11(!isAddInfinite11)
+        break;
+      case 12:
+        setIsAddInfinite12(!isAddInfinite12)
+        break;
+      case 13:
+        setIsAddInfinite13(!isAddInfinite13)
+        break;
+      case 14:
+        setIsAddInfinite14(!isAddInfinite14)
+        break;
+      case 15:
+        setIsAddInfinite15(!isAddInfinite15)
+        break;
+      case 16:
+        setIsAddInfinite16(!isAddInfinite16)
+        break;
+      case 17:
+        setIsAddInfinite17(!isAddInfinite17)
+        break;
+      case 18:
+        setIsAddInfinite18(!isAddInfinite18)
+        break;
+      case 19:
+        setIsAddInfinite19(!isAddInfinite19)
+        break;
+      case 20:
+        setIsAddInfinite20(!isAddInfinite20)
+        break;
+      case 21:
+        setIsAddInfinite21(!isAddInfinite10)
+        break;
+      case 22:
+        setIsAddInfinite22(!isAddInfinite11)
+        break;
+      case 23:
+        setIsAddInfinite23(!isAddInfinite12)
+        break;
+      case 24:
+        setIsAddInfinite24(!isAddInfinite13)
+        break;
+      case 25:
+        setIsAddInfinite25(!isAddInfinite14)
+        break;
+      case 26:
+        setIsAddInfinite26(!isAddInfinite15)
+        break;
+      case 27:
+        setIsAddInfinite27(!isAddInfinite16)
+        break;
+      case 28:
+        setIsAddInfinite28(!isAddInfinite17)
+        break;
+      case 29:
+        setIsAddInfinite29(!isAddInfinite18)
+        break;
+      case 30:
+        setIsAddInfinite30(!isAddInfinite19)
+        break;
+      case 31:
+        setIsAddInfinite31(!isAddInfinite20)
+        break;
+      case 32:
+        setIsAddInfinite32(!isAddInfinite11)
+        break;
+      case 33:
+        setIsAddInfinite33(!isAddInfinite12)
+        break;
+      case 34:
+        setIsAddInfinite34(!isAddInfinite13)
+        break;
+      case 35:
+        setIsAddInfinite35(!isAddInfinite14)
+        break;
+      case 36:
+        setIsAddInfinite36(!isAddInfinite15)
+        break;
+      case 37:
+        setIsAddInfinite37(!isAddInfinite16)
+        break;
+      case 38:
+        setIsAddInfinite38(!isAddInfinite17)
+        break;
+      case 39:
+        setIsAddInfinite39(!isAddInfinite18)
+        break;
+      case 40:
+        setIsAddInfinite40(!isAddInfinite19)
+        break;
+      case 41:
+        setIsAddInfinite41(!isAddInfinite20)
+        break;
+      case 42:
+        setIsAddInfinite42(!isAddInfinite11)
+        break;
+      case 43:
+        setIsAddInfinite43(!isAddInfinite12)
+        break;
+      case 44:
+        setIsAddInfinite44(!isAddInfinite13)
+        break;
+      case 45:
+        setIsAddInfinite45(!isAddInfinite14)
+        break;
+      case 46:
+        setIsAddInfinite46(!isAddInfinite15)
+        break;
+      case 47:
+        setIsAddInfinite47(!isAddInfinite16)
+        break;
+      case 48:
+        setIsAddInfinite48(!isAddInfinite17)
+        break;
+      case 49:
+        setIsAddInfinite49(!isAddInfinite18)
+        break;
+      case 50:
+        setIsAddInfinite50(!isAddInfinite19)
+        break;
+      case 51:
+        setIsAddInfinite51(!isAddInfinite20)
+        break;
+      default:
+        console.log("default")
+        break;
+    }
+  }
+
+  return (
+    <div className="w-full h-screen py-8 flex flex-col gap-16 overflow-x-hidden">
+      <h1 className='title'>Animation</h1>
+      <div className="flex flex-row gap-8 items-center justify-center">
+        <button onClick={() => setIsAddInfiniteAll(true)} className="btnSuccess">add infinite For All</button>
+        <button onClick={() => setIsAddInfiniteAll(false)} className="btnError">delete infinite For All</button>
+      </div>
+      <div className='w-full flex items-center justify-center flex-col gap-8 child:cursor-pointer'>
+        <p onClick={() => toggleInfinite(1)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite1 ? 'infinite' : ''} animated bounce`}>animated bounce</p>
+        <p onClick={() => toggleInfinite(2)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite2 ? 'infinite' : ''} animated flash`}>animated flash</p>
+        <p onClick={() => toggleInfinite(3)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite3 ? 'infinite' : ''} animated rubberBand`}>animated rubberBand</p>
+        <p onClick={() => toggleInfinite(4)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite4 ? 'infinite' : ''} animated shake`}>animated shake</p>
+        <p onClick={() => toggleInfinite(5)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite5 ? 'infinite' : ''} animated swing`}>animated swing</p>
+        <p onClick={() => toggleInfinite(6)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite6 ? 'infinite' : ''} animated tada`}>animated tada</p>
+        <p onClick={() => toggleInfinite(7)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite7 ? 'infinite' : ''} animated wobble`}>animated wobble</p>
+
+
+        <p onClick={() => toggleInfinite(8)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite8 ? 'infinite' : ''} animated bounceIn`}>animated bounceIn</p>
+        <p onClick={() => toggleInfinite(9)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite9 ? 'infinite' : ''} animated bounceInDown`}>animated bounceInDown</p>
+        <p onClick={() => toggleInfinite(10)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite10 ? 'infinite' : ''} animated bounceInLeft`}>animated bounceInLeft</p>
+        <p onClick={() => toggleInfinite(11)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite11 ? 'infinite' : ''} animated bounceInRight`}>animated bounceInRight</p>
+        <p onClick={() => toggleInfinite(12)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite12 ? 'infinite' : ''} animated bounceInUp`}>animated bounceInUp</p>
+
+
+        <p onClick={() => toggleInfinite(13)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite13 ? 'infinite' : ''} animated bounceOut`}>animated bounceOut</p>
+        <p onClick={() => toggleInfinite(14)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite14 ? 'infinite' : ''} animated bounceOutDown`}>animated bounceOutDown</p>
+        <p onClick={() => toggleInfinite(15)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite15 ? 'infinite' : ''} animated bounceOutLeft`}>animated bounceOutLeft</p>
+        <p onClick={() => toggleInfinite(16)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite16 ? 'infinite' : ''} animated bounceOutRight`}>animated bounceOutRight</p>
+        <p onClick={() => toggleInfinite(17)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite17 ? 'infinite' : ''} animated bounceOutUp`}>animated bounceOutUp</p>
+
+
+        <p onClick={() => toggleInfinite(18)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite18 ? 'infinite' : ''} animated fadeIn`}>animated fadeIn</p>
+        <p onClick={() => toggleInfinite(19)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite19 ? 'infinite' : ''} animated fadeInDown`}>animated fadeInDown</p>
+        <p onClick={() => toggleInfinite(20)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite20 ? 'infinite' : ''} animated fadeInDownBig`}>animated fadeInDownBig</p>
+        <p onClick={() => toggleInfinite(21)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite21 ? 'infinite' : ''} animated fadeInLeft`}>animated fadeInLeft</p>
+        <p onClick={() => toggleInfinite(22)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite22 ? 'infinite' : ''} animated fadeInLeftBig`}>animated fadeInLeftBig</p>
+        <p onClick={() => toggleInfinite(23)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite23 ? 'infinite' : ''} animated fadeInRight`}>animated fadeInRight</p>
+        <p onClick={() => toggleInfinite(24)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite24 ? 'infinite' : ''} animated fadeInRightBig`}>animated fadeInRightBig</p>
+        <p onClick={() => toggleInfinite(25)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite25 ? 'infinite' : ''} animated fadeInUp`}>animated fadeInUp</p>
+        <p onClick={() => toggleInfinite(26)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite26 ? 'infinite' : ''} animated fadeInUpBig`}>animated fadeInUpBig</p>
+
+
+        <p onClick={() => toggleInfinite(27)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite27 ? 'infinite' : ''} animated flip`}>animated flip</p>
+        <p onClick={() => toggleInfinite(28)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite28 ? 'infinite' : ''} animated flipInX`}>animated flipInX</p>
+        <p onClick={() => toggleInfinite(29)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite29 ? 'infinite' : ''} animated flipInY`}>animated flipInY</p>
+        <p onClick={() => toggleInfinite(30)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite30 ? 'infinite' : ''} animated flipOutX`}>animated flipOutX</p>
+        <p onClick={() => toggleInfinite(31)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite31 ? 'infinite' : ''} animated flipOutY`}>animated flipOutY</p>
+
+
+        <p onClick={() => toggleInfinite(32)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite32 ? 'infinite' : ''} animated lightSpeedIn`}>animated lightSpeedIn</p>
+        <p onClick={() => toggleInfinite(33)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite33 ? 'infinite' : ''} animated lightSpeedOut`}>animated lightSpeedOut</p>
+
+
+        <p onClick={() => toggleInfinite(34)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite34 ? 'infinite' : ''} animated rotateIn`}>animated rotateIn</p>
+        <p onClick={() => toggleInfinite(35)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite35 ? 'infinite' : ''} animated rotateInDownLeft`}>animated rotateInDownLeft</p>
+        <p onClick={() => toggleInfinite(36)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite36 ? 'infinite' : ''} animated rotateInDownRight`}>animated rotateInDownRight</p>
+        <p onClick={() => toggleInfinite(37)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite37 ? 'infinite' : ''} animated rotateInUpLeft`}>animated rotateInUpLeft</p>
+        <p onClick={() => toggleInfinite(38)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite38 ? 'infinite' : ''} animated rotateInUpRight`}>animated rotateInUpRight</p>
+
+
+        <p onClick={() => toggleInfinite(39)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite39 ? 'infinite' : ''} animated hinge`}>animated hinge</p>
+        <p onClick={() => toggleInfinite(40)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite40 ? 'infinite' : ''} animated rollIn`}>animated rollIn</p>
+        <p onClick={() => toggleInfinite(41)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite41 ? 'infinite' : ''} animated rollOut`}>animated rollOut</p>
+
+
+        <p onClick={() => toggleInfinite(42)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite42 ? 'infinite' : ''} animated zoomIn`}>animated zoomIn</p>
+        <p onClick={() => toggleInfinite(43)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite43 ? 'infinite' : ''} animated zoomInDown`}>animated zoomInDown</p>
+        <p onClick={() => toggleInfinite(44)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite44 ? 'infinite' : ''} animated zoomInLeft`}>animated zoomInLeft</p>
+        <p onClick={() => toggleInfinite(45)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite45 ? 'infinite' : ''} animated zoomInRight`}>animated zoomInRight</p>
+        <p onClick={() => toggleInfinite(46)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite46 ? 'infinite' : ''} animated zoomInUp`}>animated zoomInUp</p>
+
+
+        <p onClick={() => toggleInfinite(47)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite47 ? 'infinite' : ''} animated zoomOut`}>animated zoomOut</p>
+        <p onClick={() => toggleInfinite(48)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite48 ? 'infinite' : ''} animated zoomOutDown`}>animated zoomOutDown</p>
+        <p onClick={() => toggleInfinite(49)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite49 ? 'infinite' : ''} animated zoomOutLeft`}>animated zoomOutLeft</p>
+        <p onClick={() => toggleInfinite(50)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite50 ? 'infinite' : ''} animated zoomOutRight`}>animated zoomOutRight</p>
+        <p onClick={() => toggleInfinite(51)} className={`subtitle ${isAddInfiniteAll ? 'infinite' : ''} ${isAddInfinite51 ? 'infinite' : ''} animated zoomOutUp`}>animated zoomOutUp</p>
       </div>
     </div>
   )
--- src/AnimationSamples/AnimationSamples3.tsx
+++ src/AnimationSamples/AnimationSamples3.tsx
@@ -1,8 +1,71 @@
+import { useEffect } from 'react';
+import { Animate, AnimateGroup, AnimateKeyframes, useAnimate } from 'react-simple-animate';
+
+//npm i react-simple-animate
 const AnimationSamples3 = () => {
+
+  const { play, isPlaying, style } = useAnimate({
+    start: { opacity: 0 },
+    end: { opacity: 1 }
+  });
+
+  useEffect(() => {
+    play(true);
+  }, []);
+
+
   return (
     <div className="w-full h-screen py-8 flex flex-col gap-16">
       <h1 className='title'>AnimationSamples 3</h1>
-      <div className='w-full h-full flex items-center justify-center flex-col gap-8'>
+      <div className='w-full h-full flex items-center justify-center flex-col gap-8 pb-8'>
+
+        <div style={style}>
+          <h1 className='subtitle w-full'>Sample Opacity with hook</h1>
+        </div>
+
+        <span className='w-[80%] h-px bg-slate-800 my-8 opacity-50' />
+
+        <Animate play={true} start={{ opacity: 0 }} end={{ opacity: 1 }}>
+          <h1 className='subtitle w-full'>Sample Opacity with component</h1>
+        </Animate>
+
+        <span className='w-[80%] h-px bg-slate-800 my-8 opacity-50' />
+
+        <div>
+          <AnimateGroup play>
+            <Animate duration={0.5} start={{ opacity: 0 }} end={{ opacity: 1 }} sequenceIndex={0}>
+              <h3>play first</h3>
+            </Animate>
+            <Animate start={{ opacity: 0 }} end={{ opacity: 1 }} sequenceIndex={1}>
+              <h3>play second</h3>
+            </Animate>
+            <Animate start={{ opacity: 0 }} end={{ opacity: 1 }} sequenceIndex={2}>
+              <h3>play last</h3>
+            </Animate>
+          </AnimateGroup>
+        </div>
+
+        <span className='w-[80%] h-px bg-slate-800 my-8 opacity-50' />
+
+        <AnimateKeyframes
+          play
+          duration={1}
+          keyframes={['opacity: 0', 'opacity: 1']} // same as CSS @keyframes { 0% { ... } 100% { ... }}
+        >
+          <h3>AnimateKeyframes Sample 1</h3>
+        </AnimateKeyframes>
+
+        <AnimateKeyframes
+          play
+          duration={0.5}
+          keyframes={[
+            { 0: 'opacity: 0' }, // 0%
+            { 50: 'opacity: 0.5' }, // 50%
+            { 100: 'opacity: 1' } // 100%
+          ]}
+        >
+          <h3>AnimateKeyframes Sample 2</h3>
+        </AnimateKeyframes>
 
       </div>
     </div>
