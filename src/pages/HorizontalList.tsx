f78963852693bd90ede58bfe9d7a56f09036eaee
arashaltafi <altafi.a@nahad.ir>
2024-02-03 12:47:38 +0330

    a

--- src/pages/HorizontalList.tsx
+++ src/pages/HorizontalList.tsx
@@ -1,13 +1,25 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/effect-cards';
-import { EffectCards } from 'swiper/modules';
+import { EffectCards, EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
 import Carousel from "react-spring-3d-carousel";
 import { config } from "react-spring";
 import Divider from '../Components/Divider';
+const Coverflow = require('react-coverflow');
 
 //npm install swiper react-spring-3d-carousel
+//npm install react-round-carousel
+//npm install react-coverflow
+
 const HorizontalList = () => {
+
+    const handleOnClick = () => {
+
+    }
+
+    const handleOnKeyDown = () => {
+
+    }
 
     return (
         <>
@@ -33,26 +45,80 @@ const HorizontalList = () => {
             <Divider />
 
 
-            <Carousel
-                slides={[
-                    {
-                        key: 1,
-                        content: <img src="https://arashaltafi.ir/arash.jpg" />
-                    },
-                    {
-                        key: 2,
-                        content: <img src="https://arashaltafi.ir/arash.jpg" />
-                    },
-                    {
-                        key: 3,
-                        content: <img src="https://arashaltafi.ir/arash.jpg" />
-                    }
-                ]}
-                goToSlide={1}
-                offsetRadius={2}
-                showNavigation={true}
-                animationConfig={config.gentle}
-            />
+            <Coverflow
+                width={960}
+                height={480}
+                displayQuantityOfSide={3}
+                navigation={false}
+                enableHeading={true}
+                infiniteScroll={true}
+                active={0}
+            >
+                <div
+                    onClick={() => handleOnClick}
+                    onKeyDown={() => handleOnKeyDown}
+                    role="menuitem"
+                >
+                    <img src='https://arashaltafi.ir/arash.jpg' alt='title or description' style={{ display: 'block', width: '100%' }}
+                    />
+                </div>
+                <img src='https://arashaltafi.ir/arash.jpg' alt='title or description' data-action="https://arashaltafi.ir/arash.jpg" />
+                <img src='https://arashaltafi.ir/arash.jpg' alt='title or description' data-action="https://arashaltafi.ir/arash.jpg" />
+                <img src='https://arashaltafi.ir/arash.jpg' alt='title or description' data-action="https://arashaltafi.ir/arash.jpg" />
+                <img src='https://arashaltafi.ir/arash.jpg' alt='title or description' data-action="https://arashaltafi.ir/arash.jpg" />
+            </Coverflow>
+
+
+
+            <Divider />
+
+
+            <div className='w-full py-32'>
+                <Swiper
+                    effect={'coverflow'}
+                    grabCursor={true}
+                    centeredSlides={true}
+                    slidesPerView={'auto'}
+                    coverflowEffect={{
+                        rotate: 50,
+                        stretch: 0,
+                        depth: 100,
+                        modifier: 1,
+                        slideShadows: true,
+                    }}
+                    pagination={true}
+                    modules={[EffectCoverflow, Pagination]}
+                    className="swiper1"
+                >
+                    <SwiperSlide className='swiper-slide1'>
+                        <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+                    </SwiperSlide>
+                    <SwiperSlide className='swiper-slide1'>
+                        <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+                    </SwiperSlide>
+                    <SwiperSlide className='swiper-slide1'>
+                        <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+                    </SwiperSlide>
+                    <SwiperSlide className='swiper-slide1'>
+                        <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+                    </SwiperSlide>
+                    <SwiperSlide className='swiper-slide1'>
+                        <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+                    </SwiperSlide>
+                    <SwiperSlide className='swiper-slide1'>
+                        <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+                    </SwiperSlide>
+                    <SwiperSlide className='swiper-slide1'>
+                        <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+                    </SwiperSlide>
+                    <SwiperSlide className='swiper-slide1'>
+                        <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+                    </SwiperSlide>
+                    <SwiperSlide className='swiper-slide1'>
+                        <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+                    </SwiperSlide>
+                </Swiper>
+            </div>
 
         </>
     );
