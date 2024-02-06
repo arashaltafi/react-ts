0820043e8f8835a2ca53648cc9fc88a723478ee3
arashaltafi <altafi.a@nahad.ir>
2024-02-06 15:23:25 +0330

    a

--- src/Components/MenuSample.tsx
+++ src/Components/MenuSample.tsx
@@ -4,6 +4,16 @@ import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
 import Fade from '@mui/material/Fade';
+import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Tooltip from '@mui/material/Tooltip';
+import PersonAdd from '@mui/icons-material/PersonAdd';
+import Settings from '@mui/icons-material/Settings';
+import Logout from '@mui/icons-material/Logout';
 
 //npm install --save material-ui-popup-state
 const MenuSample = () => {
@@ -14,12 +24,17 @@ const MenuSample = () => {
 
     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
     const open = Boolean(anchorEl);
+    const open2 = Boolean(anchorEl);
 
     const handleClick = (event: React.MouseEvent<HTMLElement>) => {
         setAnchorEl(event.currentTarget);
     };
 
     const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const handleClose2 = () => {
         setAnchorEl(null);
     };
 
@@ -40,7 +55,7 @@ const MenuSample = () => {
 
     const handleCloseMenu = () => {
         setContextMenu(null);
-      };
+    };
 
 
     return (
@@ -103,15 +118,71 @@ const MenuSample = () => {
                     anchorReference="anchorPosition"
                     anchorPosition={
                         contextMenu !== null
-                          ? { top: contextMenu.mouseY, left: contextMenu.mouseX }
-                          : undefined
-                      }
+                            ? { top: contextMenu.mouseY, left: contextMenu.mouseX }
+                            : undefined
+                    }
                 >
                     <MenuItem onClick={handleCloseMenu}>Profile</MenuItem>
                     <MenuItem onClick={handleCloseMenu}>My account</MenuItem>
                     <MenuItem onClick={handleCloseMenu}>Logout</MenuItem>
                 </Menu>
             </div>
+
+
+
+            <>
+                <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
+                    <Typography sx={{ minWidth: 100 }}>Contact</Typography>
+                    <Typography sx={{ minWidth: 100 }}>Profile</Typography>
+                    <Tooltip title="Account settings">
+                        <IconButton
+                            onClick={handleClick}
+                            size="small"
+                            sx={{ ml: 2 }}
+                            aria-controls={open ? 'account-menu' : undefined}
+                            aria-haspopup="true"
+                            aria-expanded={open ? 'true' : undefined}
+                        >
+                            <Avatar sx={{ width: 32, height: 32 }}>M</Avatar>
+                        </IconButton>
+                    </Tooltip>
+                </Box>
+                <Menu
+                    anchorEl={anchorEl}
+                    id="account-menu"
+                    open={open2}
+                    onClose={handleClose2}
+                    onClick={handleClose2}
+                    transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                    anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                >
+                    <MenuItem onClick={handleClose2}>
+                        <Avatar /> Profile
+                    </MenuItem>
+                    <MenuItem onClick={handleClose2}>
+                        <Avatar /> My account
+                    </MenuItem>
+                    <Divider />
+                    <MenuItem onClick={handleClose2}>
+                        <ListItemIcon>
+                            <PersonAdd fontSize="small" />
+                        </ListItemIcon>
+                        Add another account
+                    </MenuItem>
+                    <MenuItem onClick={handleClose2}>
+                        <ListItemIcon>
+                            <Settings fontSize="small" />
+                        </ListItemIcon>
+                        Settings
+                    </MenuItem>
+                    <MenuItem onClick={handleClose2}>
+                        <ListItemIcon>
+                            <Logout fontSize="small" />
+                        </ListItemIcon>
+                        Logout
+                    </MenuItem>
+                </Menu>
+            </>
         </div>
     )
 }
