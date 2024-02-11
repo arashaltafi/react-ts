b4b0df7b5e314a0717b7b5ba486863c14b7669a1
Arash Altafi <arashaltafi1377@gmail.com>
2024-02-11 19:52:33 +0330

    a

--- src/Components/BestReference.tsx
+++ src/Components/BestReference.tsx
@@ -4,9 +4,13 @@ const BestReference = () => {
       <h1 className="title">best reference</h1>
 
       <div className="flex flex-col items-center justify-center gap-8 w-full">
+        <a className="py-3 px-4 text-2xl no-underline text-white rounded-xl bg-red-500" href="https://pagespeed.web.dev/" target="_blank" rel="noreferrer">pagespeed.web</a>
+        <a className="py-3 px-4 text-2xl no-underline text-white rounded-xl bg-red-500" href="https://10015.io/" target="_blank" rel="noreferrer">10015</a>
+        <a className="py-3 px-4 text-2xl no-underline text-white rounded-xl bg-red-500" href="https://coolbackgrounds.io/" target="_blank" rel="noreferrer">coolbackgrounds.io</a>
         <a className="py-3 px-4 text-2xl no-underline text-white rounded-xl bg-red-500" href="https://neumorphism.io/" target="_blank" rel="noreferrer">Neumorphism</a>
         <a className="py-3 px-4 text-2xl no-underline text-white rounded-xl bg-sky-500" href="https://uiverse.io/" target="_blank" rel="noreferrer">Best Component</a>
         <a className="py-3 px-4 text-2xl no-underline text-white rounded-xl bg-green-500" href="https://animatedbackgrounds.me/" target="_blank" rel="noreferrer">Animated Backgrounds</a>
+        <a className="py-3 px-4 text-2xl no-underline text-white rounded-xl bg-green-500" href="https://shadows.brumm.af/" target="_blank" rel="noreferrer">shadows.brumm.af</a>
       </div>
     </div>
   )
