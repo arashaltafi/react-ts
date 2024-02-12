9ce51f43e6fdde73eb604bc2aeec11ed232d1ee7
arashaltafi <altafi.a@nahad.ir>
2024-02-12 15:23:15 +0330

    a

--- src/pages/SpringSample.tsx
+++ src/pages/SpringSample.tsx
@@ -26,6 +26,17 @@ const SpringSample = () => {
         reverse: true
     })
 
+    const springs2 = useSpring({
+        from: { background: '#ff6d6d', y: -40, x: 0 },
+        to: [
+            { x: 80, background: '#fff59a' },
+            { y: 40, background: '#88DFAB' },
+            { x: 0, background: '#569AFF' },
+            { y: -40, background: '#ff6d6d' },
+        ],
+        loop: true,
+    })
+
     const [trails] = useTrail(
         2,
         () => ({
@@ -104,6 +115,21 @@ const SpringSample = () => {
                 ))
             }
             </div>
+
+
+
+            <animated.div
+                style={{
+                    width: 40,
+                    height: 40,
+                    borderRadius: 4,
+                    ...springs,
+                }}
+            >
+                spring
+            </animated.div>
+
+
 
         </div>
     )
