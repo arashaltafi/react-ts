76420e5052a6590971b4ca05fcec60904e708477
arashaltafi <altafi.a@nahad.ir>
2024-03-16 10:33:18 +0330

    a

--- src/pages/ValidateSample.tsx
+++ src/pages/ValidateSample.tsx
@@ -1,8 +1,12 @@
 import { useState } from "react";
+import Divider from "../Components/Divider";
 
 const ValidateSample = () => {
     const [isValid, setIsValid] = useState<boolean | null>(null);
     const [nationalCode, setNationalCode] = useState<string>('');
+
+    const [isValidPhone, setIsValidPhone] = useState<boolean | null>(null);
+    const [phone, setPhone] = useState<string>('');
 
     const isValidNationalCode = (code: string) => {
         if (code.length !== 10 || /(\d)(\1){9}/.test(code)) {
@@ -22,18 +26,40 @@ const ValidateSample = () => {
         setIsValid(+chars[9] === lastDigit);
     }
 
+    const isValidPhoneNumber = (phone: string) => {
+        const iranianPhoneRegex = /^09\d{9}$/;
+        setIsValidPhone(iranianPhoneRegex.test(phone));
+    }
+
     return (
-        <div className="w-full h-screen py-8 flex flex-col gap-16">
-            <h1 className='title'>ValidateSample</h1>
-            <div className='w-full h-full flex items-center justify-center flex-col gap-8'>
-                <input
-                    className='w-1/3 px-4 py-3 border-2 border-solid border-black m-4 rounded-xl shadow-lg'
-                    type="text"
-                    placeholder='Enter Your NationalCode ...'
-                    onChange={(e) => setNationalCode(e.target.value)}
-                />
-                <p className={`text-2xl font-bold text-center ${isValid === null ? 'text-black' : isValid ? 'text-green-500' : 'text-red-500'}`}>{isValid !== null && (isValid ? 'Valid' : 'Invalid')}</p>
-                <button className='btnSuccess' onClick={() => isValidNationalCode(nationalCode)}>Check Validation NationalCode</button>
+        <div className="w-full py-8 flex flex-col gap-16">
+            <h1 className='title text-red-500'>ValidateSample</h1>
+            <div className='w-full h-full flex items-center justify-center flex-col gap-12 mt-16'>
+                <div className="w-full h-full flex items-center justify-center flex-col gap-6">
+                    <h3 className='subtitle'>Validate NationalCode</h3>
+                    <input
+                        className='w-1/3 px-4 py-3 border-2 border-solid border-black m-4 rounded-xl shadow-lg'
+                        type="text"
+                        placeholder='Enter Your NationalCode ...'
+                        onChange={(e) => setNationalCode(e.target.value)}
+                    />
+                    <p className={`text-2xl font-bold text-center ${isValid === null ? 'text-black' : isValid ? 'text-green-500' : 'text-red-500'}`}>{isValid !== null && (isValid ? 'Valid' : 'Invalid')}</p>
+                    <button className='btnSuccess' onClick={() => isValidNationalCode(nationalCode)}>Check Validation NationalCode</button>
+                </div>
+
+                <Divider />
+
+                <div className="w-full h-full flex items-center justify-center flex-col gap-6">
+                    <h3 className='subtitle'>Validate Phone</h3>
+                    <input
+                        className='w-1/3 px-4 py-3 border-2 border-solid border-black m-4 rounded-xl shadow-lg'
+                        type="text"
+                        placeholder='Enter Your Phone ...'
+                        onChange={(e) => setPhone(e.target.value)}
+                    />
+                    <p className={`text-2xl font-bold text-center ${isValidPhone === null ? 'text-black' : isValidPhone ? 'text-green-500' : 'text-red-500'}`}>{isValidPhone !== null && (isValidPhone ? 'Valid' : 'Invalid')}</p>
+                    <button className='btnSuccess' onClick={() => isValidPhoneNumber(phone)}>Check Validation Phone</button>
+                </div>
             </div>
         </div>
     )
